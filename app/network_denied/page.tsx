7ae890d85133891c25a7a48f4aec9ac3ce93fb5c
Samad Ghanbari <ghanbari.samad@gmail.com>
2024-01-14 00:19:54 +0330

    denied page use rootLayout

--- app/network_denied/page.tsx
+++ app/network_denied/page.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
-import ac_image from "./../../public/image/forbidden.gif";
+import ad_gif from "/public/image/denied/forbidden.gif";
+import ad from "/public/image/denied/denied.png";
+import Layout from "./layout";
 
-export default function Home() {
+export default function accessDenied() {
 	return (
-		<div className=" h-screen w-full bg-red-200 text-red-500 text-2xl font-bold border-red-700 border-2 flex justify-center items-center">
+		<div className=" h-screen w-full bg-red-200 text-red-500 font-bold border-red-700 border-y-8 flex justify-center items-center">
 			<Image
 				alt="access denied user"
-				src={ac_image}
+				src={ad_gif}
 				layout="fill"
 				objectFit="cover"
 				className="absolute blur-lg opacity-20 z-0"
 			/>
 
-			<div className="p-10 text-center font-irsans z-10 drop-shadow-2xl">
+			<div className="p-10 text-center font-irsans z-10 drop-shadow-2xl text-sm sm:text-lg md:text-xl lg:text-3xl">
 				دسترسی به سامانه برای شما مقدور نمی‌باشد
+				<Image
+					alt="denied"
+					src={ad}
+					width={128}
+					height={128}
+					className="block mx-auto mt-10"
+				/>
 				<p
-					className="leading-loose font-bold p-10 text-2xl font-salsa drop-shadow-lg"
+					className="leading-loose font-bold p-10 text-lg sm:text-xl md:text-2xl lg:text-3xl font-salsa drop-shadow-lg"
 					dir="ltr">
 					Network Access Denied!
 				</p>
